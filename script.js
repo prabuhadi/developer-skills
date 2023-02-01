f8a6c1e44d3dd026694af46deb15b1c8109c9982
prabuhadi <prabuhadiyo@gmail.com>
2023-02-01 16:29:29 +0700

    Create snippets and setting extension

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 'test';
+if (x === 'test') console.log('test complete');
+
+const calcAge = birthYear => 2032 - birthYear;
+
+console.log();
+// BUG
