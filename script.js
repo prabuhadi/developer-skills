f4a949da71c093fbd5e58a0a799e8a3526356691
prabuhadi <prabuhadiyo@gmail.com>
2023-02-02 14:24:41 +0700

    Debugging with console and breakpoints

--- script.js
+++ script.js
@@ -78,3 +78,59 @@ const calcTempAmplitudeNew = function () {
 };
 
 calcTempAmplitudeNew(temperatures);
+
+// NEXT PHASE
+/*
+Convert current temperature celcius to kelvin (absolute temperature)
+*/
+// console.log('This is normal');
+// console.warn('This is warning');
+// console.error('This is error');
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+
+    // C) FIX
+    // value: parseInt(prompt('Degree celsius: ')), // Output of prompt always string
+    value: 26,
+  };
+  // B) FIND
+  console.table(measurement);
+  console.log(typeof measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) IDENTIFY BUG
+console.log(measureKelvin());
+
+// D) DEBUGGING
+// Output value is not a number, convert string into number
+// use Number() or parseInt()
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    // debugger;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
+
+// 1) IDENTIFY
+// min = 0 is the lowest number compare to array
